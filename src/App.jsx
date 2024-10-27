5ed5678dd4cfccde3b1de05a6d9e640f38ba60fd
Anthony <zyxanthony15@gmail.com>
2024-10-27 11:06:01 +0800

    my first  commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import LevelOne from "./components/levels/LevelOne";
+import NavBar from "./components/NavBar";
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <LevelOne name={"Anthony"}/>
+    </>
+  )
+}
---
+++ src/components/levels/LevelOne.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function LevelOne(props) {
+  return (
+    <div>Hello, {props.name}</div>
+  )
+}
